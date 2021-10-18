6b19dce0368bddd052c2973d6d2daea326040b94
SFPXV0O <SFPXV0O@post.at>
2021-10-18 13:02:03 +0000

    Fixing library

---
+++ ServiceBus.Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceBus.Model
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+    }
+}
